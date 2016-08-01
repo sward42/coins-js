94853d829d18f7abaf89e574a44b769445a403de
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-08-01 13:47:20 -0500

    initial commit

---
+++ coins.js
@@ -0,0 +1,23 @@
+/*
+  Input: 0.67
+  Output:
+  {
+    quarters: 2,
+    dimes: 1,
+    nickels: 1,
+    pennies: 2
+  }
+
+*/
+
+function coinCounter () {
+  // Initialize a JavaScript object to hold the coins
+  var coinPurse = {};
+
+  coinPurse.quarters = 0;
+
+  return coinPurse;
+}
+
+var coins = coinCounter()
+console.log();
