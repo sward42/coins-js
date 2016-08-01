c08750986a3854c1507be94fcb0ac94efe650781
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-08-01 15:14:22 -0500

    long math

--- coins.js
+++ coins.js
@@ -10,14 +10,42 @@
 
 */
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
+// Initialize a JavaScript object to hold the coins
   var coinPurse = {};
+  var inputAmt ="";
 
-  coinPurse.quarters = 0;
+function coinCounter (inputAmt) {
+
+//var q  = inputAmt/.25; 
+var numQuarters = parseInt(inputAmt/.25); 
+
+inputAmt = inputAmt - (numQuarters * .25);
+
+//var d = inputAmt/ .10;
+var numDimes = parseInt(inputAmt/.10);
+
+inputAmt = inputAmt - (numDimes * .10);
+
+//var n = inputAmt/ .05;
+var numNickels = parseInt(inputAmt/.05);
+
+inputAmt = inputAmt - (numNickels * .05);
+
+//var p = inputAmt/ .01;
+var numPennies = parseInt(inputAmt/.01);
+  
+
+  coinPurse.quarters = numQuarters;
+  coinPurse.dimes = numDimes;
+  coinPurse.nickels = numNickels;
+  coinPurse.pennies = numPennies;
 
   return coinPurse;
 }
 
 var coins = coinCounter()
-console.log();
+console.log(coinPurse);
+
+
+
+
